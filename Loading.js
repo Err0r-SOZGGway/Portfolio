9a71c044b1d9c55495723444f12e51feb9c44b15
Err0r-SOZGGway <syado6664@gmail.com>
2023-06-29 23:58:24 +0900

    サイトのローディング画面の時間を短縮

--- Loading.js
+++ Loading.js
@@ -1,13 +1,13 @@
 $(function(){
   $(window).on('load',function(){
     //ローディングアニメーションをフェードアウト
-    $('.loader').delay(600).fadeOut(600);
+    $('.loader').delay(300).fadeOut(300);
     //背景色をフェードアウト
-    $('.loader-bg').delay(900).fadeOut(800);
+    $('.loader-bg').delay(600).fadeOut(400);
 });
 
   //ページの読み込みが完了してなくても5秒後にアニメーションを非表示にする
   setTimeout(function(){
-    $('.loader-bg').fadeOut(600);
-  },5000);
+    $('.loader-bg').fadeOut(400);
+  },4000);
 });
