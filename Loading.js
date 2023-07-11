c1cf6ef74f48dff48fc0c08086815afec97ae578
Err0r-SOZGGway <syado6664@gmail.com>
2023-07-11 22:14:50 +0900

    ローディング時間の短縮と画面のコンテンツの位置を調整

--- Loading.js
+++ Loading.js
@@ -1,13 +1,13 @@
 $(function(){
   $(window).on('load',function(){
     //ローディングアニメーションをフェードアウト
-    $('.loader').delay(300).fadeOut(300);
+    $('.loader').delay(200).fadeOut(200);
     //背景色をフェードアウト
-    $('.loader-bg').delay(600).fadeOut(400);
+    $('.loader-bg').delay(200).fadeOut(200);
 });
 
-  //ページの読み込みが完了してなくても5秒後にアニメーションを非表示にする
+  //ページの読み込みが完了してなくても4秒後にアニメーションを非表示にする
   setTimeout(function(){
-    $('.loader-bg').fadeOut(400);
+    $('.loader-bg').fadeOut(200);
   },4000);
 });
